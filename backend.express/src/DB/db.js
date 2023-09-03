c895c76b5c05def73bb0556b701bdbd92c7b47fb
chunyean <chunyeanfoong@gmail.com>
2023-09-03 23:43:51 +0800

    Update db.js

--- backend.express/src/DB/db.js
+++ backend.express/src/DB/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.USERNAME,
+  user: process.env.NAME,
   host: process.env.HOST,
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
