2972394f9efdf0520c97d1e7e57e13cba4d151f9
chunyean <chunyeanfoong@gmail.com>
2023-09-04 22:54:11 +0800

    edit code for all resgister, login at employee.js

--- backend.express/src/controller/employee.js
+++ backend.express/src/controller/employee.js
@@ -10,22 +10,12 @@ const register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
 
     // insert the customer detail into database
-    const newEmployee = await pool.query(
-      $`insert into employees (name, password, contact) values (${req.body.name}, ${hashedPassword}, ${req.body.contact})`
+    await pool.query(
+      `insert into employees (name, password, contact) values ($1, $2, $3)`,
+      [req.body.name, hashedPassword, req.body.contact]
     );
 
-    // display the last inserted record
-    if (newEmployee.rows.length > 0) {
-      const lastRow = newEmployee.rows[newEmployee.rows.length - 1];
-      res.json(lastRow);
-    } //when only one row is returned in the result
-    else if (newEmployee.rows.length == 0) {
-      const firstRow = newEmployee.rows[0];
-      res.json(firstRow);
-    } else {
-      // when no rows inside the result will show error
-      res.status(404).json({ error: "No rows found" });
-    }
+    res.json({ status: "ok", message: "new employee created" });
   } catch (error) {
     console.log(error.message);
     res.json({ status: "error", message: error.message });
@@ -34,30 +24,33 @@ const register = async (req, res) => {
 
 // get next available employee id for registration
 const nextAvaiId = async (req, res) => {
-    try {
-        //increase the sequence by 1
-        await pool.query("SELECT nextval('employee_id_seq')");
-        
-        // retrieve the next available id
-        const result = await pool.query("SELECT 'SEI' || currval('employee_id_seq') AS next_id");
-        res.json(result.rows[0].next_id);
-    } catch (error) {
-        console.log(error.message);
-        res.status(400).json({ status: "error", message: error.message });
-    }
-}
+  try {
+    //increase the sequence by 1
+    await pool.query("SELECT nextval('employee_id_seq')");
+
+    // retrieve the next available id
+    const result = await pool.query(
+      "SELECT 'SEI' || currval('employee_id_seq') AS next_id"
+    );
+    res.json(result.rows[0].next_id);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ status: "error", message: error.message });
+  }
+};
 
 // employee login
 const login = async (req, res) => {
   try {
-    const employee = await pool.query(
-      $`select * from employees where id = req.body.employee_id`
-    );
+    const result = await pool.query(`select * from employees where id = $1`, [
+      req.body.id,
+    ]);
 
-    if (!employee) {
+    if (!result) {
       return res.status(400).json({ message: "ID does not exist" });
     }
-
+    console.log(result)
+    const employee = result.rows[0]
     const correctPassword = await bcrypt.compare(
       req.body.password,
       employee.password
@@ -66,7 +59,7 @@ const login = async (req, res) => {
     if (correctPassword) {
       const payload = {
         id: employee.id,
-        name: employee.name
+        name: employee.name,
       };
 
       const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
@@ -74,7 +67,7 @@ const login = async (req, res) => {
         jwtid: uuidv4(),
       });
 
-      const refresh = jwt.sign(claims, process.env.REFRESH_SECRET, {
+      const refresh = jwt.sign(payload, process.env.REFRESH_SECRET, {
         expiresIn: "30d",
         jwtid: uuidv4(),
       });
@@ -91,23 +84,22 @@ const login = async (req, res) => {
 
 // use refresh token to get new access token
 const refresh = async (req, res) => {
-    try {
-        const decoded = jwt.verify(req.body.refresh, process.env.REFRESH_SECRET);
-        const payload = {
-            id: decoded.id,
-            name: decoded.name
-        };
-        const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
-            expiresIn: "1d",
-            jwtid: uuidv4()
-        })
+  try {
+    const decoded = jwt.verify(req.body.refresh, process.env.REFRESH_SECRET);
+    const payload = {
+      id: decoded.id,
+      name: decoded.name,
+    };
+    const access = jwt.sign(payload, process.env.ACCESS_SECRET, {
+      expiresIn: "1d",
+      jwtid: uuidv4(),
+    });
 
-        res.json({access, payload})
-    } catch (error) {
-        console.log(error.message);
-        res.status(400).json({ status: "error", message: "token refresh error" });
-        
-    }
-}
+    res.json({ access, payload });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ status: "error", message: "token refresh error" });
+  }
+};
 
 module.exports = { register, nextAvaiId, login, refresh };
