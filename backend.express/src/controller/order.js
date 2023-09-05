1565a72cc4a6e25409b35ddd89ee6f694423e56b
chunyean <chunyeanfoong@gmail.com>
2023-09-06 00:14:43 +0800

    solve the problem of create new order

--- backend.express/src/controller/order.js
+++ backend.express/src/controller/order.js
@@ -3,6 +3,7 @@
 const pool = require("../DB/db");
 
 const newOrder = async (req, res) => {
+  //add pax
   try {
     const {
       customer_id,
@@ -10,55 +11,48 @@ const newOrder = async (req, res) => {
       fnb_item_list_id,
       quantity,
       order_type,
-      pax,
-      id,
     } = req.body;
 
-    // console.log(customer_id);
-    // console.log(table_number);
-    // console.log(fnb_item_list_id);
-    // console.log(quantity);
-    // console.log(order_type);
-    // console.log(pax);
-
-    // const price = await pool.query(
-    //   "select price from fnb_item_lists where id = $1",
-    //   [fnb_item_list_id]
-    // );
-    // console.log("ok");
-
-    // const totalPrice = price * quantity;
-    // console.log("ok3");
+    // use table_number and customer_id to create new row of orders table
+    // get the new generate order.id to create another new row at table fnb_order_lists
     const create = await pool.query(
       "insert into orders (table_number, customer_id) values ($1, $2) returning id",
       [table_number, customer_id]
     );
-    console.log("ok2");
-    const result = await pool.query(
-      "insert into fnb_order_lists (order_id, pax, fnb_item_list_id, quantity, total_price, order_type) values ($1, $2, $3, $4, $5, $6) returning *",
-      [
-        create,
-        pax,
-        fnb_item_list_id,
-        quantity,
-        totalPrice,
-        order_type.toUpperCase(),
-      ]
-    );
+    const newID = create.rows[0].id;
 
-    res.json(result.rows[0]);
+    //use fnb_item_list_id to retrieve each individual price
+    const list = await pool.query(
+      "select price from fnb_item_lists where id = any($1)",
+      [fnb_item_list_id]
+    );
+    const unitPrice = list.rows;
+
+    // use for loop to get index of array
+    // use index to get the data of unitPrice, quantity, fnb_item_list_id, each of it contain the same number of length of an array
+    for (let index = 0; index < quantity.length; index++) {
+      const price = unitPrice[index].price * quantity[index];
+      await pool.query(
+        "insert into fnb_order_lists (total_price, quantity, order_id, order_type, fnb_item_list_id) values ($1, $2, $3, $4, $5)",
+        [price, quantity[index], newID, order_type, fnb_item_list_id[index]]
+      );
+    }
+
+    res.json({ status: "ok", message: "Order has been created" });
   } catch (error) {
     console.log(error.message);
-    res.json({ status: "error", message: error.message });
+    res.json({ status: "error", message: "wrong" });
   }
 };
 
 const deleteOrderedItem = async (req, res) => {
   try {
-    await pool.query("delete from fnb_order_lists where id = $1", [
-      req.body.id,
-    ]);
-    res.json({ status: "ok", message: "item has been deleted" });
+    const result = await pool.query(
+      "delete from fnb_order_lists join orders where orders.id = order_id and fnb_item_list_id = $1 and table_number = $2",
+      [[req.body.id, req.body.table_number]]
+    );
+
+    res.json(result);
   } catch (error) {
     console.log(error.message);
     res.json({ status: "error", message: error.message });
