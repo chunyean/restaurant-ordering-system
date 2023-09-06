15309e741f93bda68a8f7576f73c45c6ba4b5d52
chunyean <chunyeanfoong@gmail.com>
2023-09-07 00:45:55 +0800

    edit code and add comment for all the code

--- backend.express/src/controller/payment.js
+++ backend.express/src/controller/payment.js
@@ -1,23 +1,30 @@
-//create API for payment
+//create conteoller for payment
 
 const pool = require("../DB/db");
 
+// create new payment
+// 1st: insert employee id to create new row under table payments and get the new payment id
+// 2nd: use table number to retrieve all order id from table orders which haven't pay
+// 3rd: use for loop to insert payment id and all order id into table order_payment
+// 4th: retrieve total_price from table order_payment and calculate the nett amount
+// 5th: calculate all the gst, service charge and total amount and insert back into payments
 const newPayment = async (req, res) => {
   try {
+    //1st
     const create = await pool.query(
       "insert into payments (employee_id) values ($1) returning id",
-      [req.params.id]
+      [req.userID]
     );
-
     const paymentID = create.rows[0].id;
 
+    //2nd
     const order = await pool.query(
       "select id from orders where table_number=$1 and is_payment=false",
       [req.body.table_number]
     );
-
     const orderID = order.rows;
 
+    //3rd
     for (let idx = 0; idx < orderID.length; idx++) {
       await pool.query(
         "insert into order_payment (payment_id, order_id) values ($1,$2)",
@@ -25,22 +32,24 @@ const newPayment = async (req, res) => {
       );
     }
 
+    //4th
     const price = await pool.query(
-      "select total_price from order_payment join orders on orders.id = order_payment.order_id join fnb_order_lists on fnb_order_lists.order_id = orders.id where payment_id=$1",
+      "select total_price from order_payment join orders on orders.id = order_payment.order_id join order_lists on order_lists.order_id = orders.id where payment_id=$1",
       [paymentID]
     );
 
-    let totalPrice = 0;
+    let nettAmount = 0;
     for (let idx = 0; idx < price.rows.length; idx++) {
-      totalPrice += Number(price.rows[idx].total_price);
+      nettAmount += Number(price.rows[idx].total_price);
     }
 
-    const serviceCharge = totalPrice * 0.1;
+    const serviceCharge = nettAmount * 0.1;
 
-    const gst = (totalPrice + serviceCharge) * 0.08;
+    const gst = (nettAmount + serviceCharge) * 0.08;
 
-    const finalAmount = totalPrice + serviceCharge + gst;
+    const finalAmount = nettAmount + serviceCharge + gst;
 
+    //5th
     const finalPayment = await pool.query(
       "update payments set nett_amount=$1, gst=$2, service_charge=$3, total_amount=$4 returning *",
       [
@@ -59,6 +68,7 @@ const newPayment = async (req, res) => {
   }
 };
 
+// completed payment will change status unsed table payments is_completed to true and table order is_payemnt to true
 const completedPayment = async (req, res) => {
   try {
     await pool.query("update payments set is_completed=true where id=$1", [
@@ -77,6 +87,11 @@ const completedPayment = async (req, res) => {
         orderID[idx].order_id,
       ]);
     }
+
+    await pool.query("insert into order_histories (history) value ($1)", [
+      req.params.id,
+    ]);
+    
     res.json({ status: "ok", message: "Payment has been completed" });
   } catch (error) {
     console.log(error.message);
