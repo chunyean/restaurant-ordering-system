0c9033ca5b4f775053ba8b7044b26fee7f6473b7
chunyean <chunyeanfoong@gmail.com>
2023-09-07 15:03:10 +0800

    edit validataion and payload for employee.js

--- backend.express/src/validators/employee.js
+++ backend.express/src/validators/employee.js
@@ -8,21 +8,19 @@ const validateStaffRegister = [
     .isLength({
       min: 8,
       max: 20,
-    })
-    .matches(/^[a-zA-Z0-9]+$/, "Password must be alphanumeric"),
+    }),
   body("contact", "contact number is required").not().isEmpty(),
 ];
 
 const validateStaffLogin = [
-  body("username", "Username is required.").not().isEmpty(),
+  body("id", "ID is required.").not().isEmpty(),
   body("password", "Password is required")
     .not()
     .isEmpty()
     .isLength({
       min: 8,
       max: 20,
-    })
-    .matches(/^[a-zA-Z0-9]+$/, "Password must be alphanumeric"),
+    }),
 ];
 
 module.exports = { validateStaffLogin, validateStaffRegister };
