6ea371720cf126bc8e9d2503e1df0426bb4de2b0
chunyean <chunyeanfoong@gmail.com>
2023-09-08 13:11:03 +0800

    add in login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
 import UserContext from "./components/context/user";
 import Header from "./components/customer/Header";
+import Register from "./components/customer/Register";
+import Login from "./components/customer/Login";
 
 const App = () => {
   const [accessToken, setAccessToken] = useState("");
-  const [register, setRegister] = useState(true);
-  const [login, setLogin] = useState(false);
+  const [header, setHeader] = useState(true);
+  const [register, setRegister] = useState(false);
+  const [login, setLogin] = useState(true);
 
   return (
     <>
       <UserContext.Provider value={{ accessToken, setAccessToken }}>
-        <Header register={register} setRegister={setRegister} login={login}setLogin={setLogin}></Header>
+        <Header setRegister={setRegister} setLogin={setLogin}></Header>
+        {register && (
+          <Register setRegister={setRegister} setLogin={setLogin}></Register>
+        )}
+        {login && <Login setRegister></Login>}
       </UserContext.Provider>
     </>
   );
