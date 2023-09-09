7a50b4b8673b0fe5ac270527fa4688634d29291e
chunyean <chunyeanfoong@gmail.com>
2023-09-09 15:25:47 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,31 @@ import UserContext from "./components/context/user";
 import Header from "./components/customer/Header";
 import Register from "./components/customer/Register";
 import Login from "./components/customer/Login";
+import MenuPage from "./components/fnb_item/MenuPage";
 
 const App = () => {
   const [accessToken, setAccessToken] = useState("");
-  const [header, setHeader] = useState(true);
-  const [register, setRegister] = useState(false);
-  const [login, setLogin] = useState(true);
+  const [register, setRegister] = useState(true);
+  const [login, setLogin] = useState(false);
+  const [user, setUser] = useState("");
+  const [foodPage, setFoodPage] = useState(true);
+  const [beveragePage, setBeveragePage] = useState(false);
 
   return (
     <>
       <UserContext.Provider value={{ accessToken, setAccessToken }}>
-        <Header setRegister={setRegister} setLogin={setLogin}></Header>
+        {/* <Header setRegister={setRegister} setLogin={setLogin}></Header>
         {register && (
           <Register setRegister={setRegister} setLogin={setLogin}></Register>
         )}
-        {login && <Login setRegister></Login>}
+        {login && <Login setRegister={setRegister} setUser={setUser}></Login>} */}
+        <MenuPage
+          user={user}
+          foodPage={foodPage}
+          setFoodPage={setFoodPage}
+          beveragePage={beveragePage}
+          setBeveragePage={setBeveragePage}
+        ></MenuPage>
       </UserContext.Provider>
     </>
   );
