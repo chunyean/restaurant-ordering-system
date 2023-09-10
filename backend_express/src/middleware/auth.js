a7022dddc91b794a6f1d9b7e4793543ac9d41dd0
chunyean <chunyeanfoong@gmail.com>
2023-09-11 00:58:42 +0800

    add test token for testing

--- backend_express/src/middleware/auth.js
+++ backend_express/src/middleware/auth.js
@@ -5,7 +5,11 @@ const auth = (req, res, next) => {
     return res.status(400).json({ status: "error", msg: "no token found" });
   }
 
-  const token = req.headers["authorization"].replace("Bearer ", "");
+  // const token = req.headers["authorization"].replace("Bearer ", "");
+
+  const test =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0SUQiOjIwMDAyLCJ1c2VybmFtZSI6InBpa2FjaHUxIiwiaWF0IjoxNjk0MzIzNjAyLCJleHAiOjE2OTQ0MTAwMDIsImp0aSI6ImQ4NmUyZmE4LWY5NzEtNDQzNC05Zjk3LTJhZDU5NzE5NWExNCJ9.NuxdGvBvc9L6GTXE9PWstiByASPevbQExRyBm4ZoE-M";
+  const token = test;
 
   if (token) {
     try {
@@ -15,7 +19,6 @@ const auth = (req, res, next) => {
       req.staffID = decoded.id;
 
       next();
-      
     } catch (error) {
       return res
         .status(401)
